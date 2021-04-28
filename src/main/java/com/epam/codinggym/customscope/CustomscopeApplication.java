167b4f1c0c27e9a33d611d8d0c574b12f626e85c
Romin Nuro <romin_nuro@epam.com>
2021-04-28 15:40:55 +0300

    Initial commit

---
+++ src/main/java/com/epam/codinggym/customscope/CustomscopeApplication.java
@@ -0,0 +1,13 @@
+package com.epam.codinggym.customscope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomscopeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomscopeApplication.class, args);
+    }
+
+}
